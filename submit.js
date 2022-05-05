d53d2ded77c94239074fb615d5d14851e8144a32
sielavic <63396872+sielavic@users.noreply.github.com>
2022-05-05 17:48:46 +0300

    Update submit.js

--- submit.js
+++ submit.js
@@ -2,9 +2,11 @@
             e.preventDefault();
             var task_id = $('input[name*="task_id"]').val();
             var folder_name = $('input[name*="folder_name"]').val();
-            
-            var formData = new FormData();
-            formData.append('file', $("#js-file_"+card_id_pop)[0].files[0]);
+
+            var fd = new FormData();
+            fd.append('file', $("#js-file_"+card_id_pop)[0].files[0]);
+            fd.append('task_id',task_id);
+            fd.append('folder_name',folder_name);
 
             $.ajax({
                   type: 'POST',
@@ -12,7 +14,7 @@
                   cache: false,
                   contentType: false,
                   processData: false,
-                  data:formData,
+                  data:fd,
                   dataType : 'json',
                   success: function () {
                         alert('form was submitted');
